0bd4890b1c18bf23836de1309c586b0d4a45d067
KaykyAdriano <conteudokayky@gmail.com>
2026-01-13 17:58:27 -0300

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -386,11 +386,11 @@ function atualizarCorDoHorario() {
 
 }
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
+    console.log(`Servidor rodando na porta ${PORT}`);
 })
+
 
 
 
